9b531cc0210bd49d0f95560fd2e004efce5ca596
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-22 09:47:00 -0700

    fixed app error from heroku

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,15 +1,32 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
+diagnostics.get("/", (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  console.info(`${req.method} request received for diagnostics`);
+
+  readFromFile("./db/diagnostics.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST Route for a error logging
-diagnostics.post('/', (req, res) => {
+diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.info(`${req.method} request received to submit a diagnostics`);
+  // get the tip, topic & username from the body
+  // packag them up in a new diagnostics object
+  const newDiagnostics = {
+    time: Date.now(),
+    error_id: uuidv4(),
+    errors: req.body,
+  };
+  res.json(newDiagnostics);
+  console.log("New diagnostics: ", newDiagnostics);
+
+  readAndAppend(newDiagnostics, "./db/diagnostics.json");
 });
 
 module.exports = diagnostics;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
+const tipsRouter = require("./tips");
+const feedbackRouter = require("./feedback");
+const diagnosticRouter = require("./diagnostics");
 // TODO: import your diagnostics route
 
-router.use('/tips', tipsRouter);
-router.use('/feedback', feedbackRouter);
+router.use("/tips", tipsRouter);
+router.use("/feedback", feedbackRouter);
 // TODO: Initialize diagnostics route
+router.use("/diagnostics", diagnosticRouter);
 
 module.exports = router;
